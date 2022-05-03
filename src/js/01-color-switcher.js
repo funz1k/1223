cd7bc952a1f37f302cc5158a834c20773c741ff4
funz1k <92177445+funz1k@users.noreply.github.com>
2022-05-03 18:57:08 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    body: document.querySelector('body'),
+    starBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]')
+}
 
+let timeoutID = null;
+
+refs.stopBtn.setAttribute('disabled', 'disabled')
+refs.starBtn.addEventListener('click', onStartSwitchColor);
+refs.stopBtn.addEventListener('click', onStopSwitchColor);
+
+function onStartSwitchColor(e) {
+    timeoutID = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+    refs.starBtn.setAttribute('disabled', 'disabled')
+    refs.stopBtn.removeAttribute('disabled', 'disabled')
+}
+
+
+function onStopSwitchColor(e) {
+    clearInterval(timeoutID)
+    refs.stopBtn.setAttribute('disabled', 'disabled')
+    refs.starBtn.removeAttribute('disabled', 'disabled')
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
